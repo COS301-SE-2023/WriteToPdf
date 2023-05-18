e4d8e08bde4a8af2bc6101adbdb49c2c8754b9f9
JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>
2023-05-18 20:08:52 +0200

    Created Login page and changed app title

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
