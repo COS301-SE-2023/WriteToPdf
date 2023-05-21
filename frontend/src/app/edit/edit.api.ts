61f95a56e698126229178ef77b10fe2f6d827be4
Dylan Kapnias <u18108467@tuks.co.za>
2023-05-21 22:35:39 +0200

    Added interface to frontend

--- frontend/src/app/edit/edit.api.ts
+++ frontend/src/app/edit/edit.api.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { ITest } from '../../../../backend/src/api-test/interfaces';
+import { ITest } from './interfaces';
 
 @Injectable({
   providedIn: 'root',
@@ -13,6 +13,8 @@ export class EditApi {
   constructor(private http: HttpClient) {}
 
   getLoremIpsum(): Observable<ITest> {
-    return this.http.get<ITest>(`${this.apiUrl}/test`);
+    return this.http.get<ITest>(
+      `${this.apiUrl}/test`,
+    );
   }
 }
