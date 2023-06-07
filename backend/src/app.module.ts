08b44267a431546e777249d8fe9e71417993c6bc
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-06-07 18:21:49 +0200

    Extracted code to typeorm.config file ->typeOrmConfig

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,27 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiTestModule } from './api-test/api-test.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
-// Database connection credentials go in here
-// need to manage password with AWS secrets
+import 'dotenv/config';
 
 @Module({
-    imports: [
-      TypeOrmModule.forRoot({
-        type: 'mariadb',
-        host: 'YOUR_RDS_HOSTNAME',
-        port: 3306,
-        username: 'YOUR_USERNAME',
-        password: 'YOUR_PASSWORD',
-        database: 'YOUR_DATABASE_NAME',
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true, // Set to false in production to prevent automatic schema synchronization
-      }),
-      ApiTestModule,
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    ApiTestModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
-
-
