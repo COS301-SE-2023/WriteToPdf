49b5e7fd562a08008a51d307b1cc0cc1d1b83454
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-06-09 12:58:46 +0200

    Imported new entity modules created via  Nest cli

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiTestModule } from './api-test/api-test.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
+import { dataSourceOptions } from 'db/data-source';
+import { UsersModule } from './users/users.module';
+import { MarkdownFilesModule } from './markdown_files/markdown_files.module';
+import { AssetsModule } from './assets/assets.module';
+import { FoldersModule } from './folders/folders.module';
 
 import 'dotenv/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forRoot(dataSourceOptions),
     ApiTestModule,
+    UsersModule,
+    MarkdownFilesModule,
+    AssetsModule,
+    FoldersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
