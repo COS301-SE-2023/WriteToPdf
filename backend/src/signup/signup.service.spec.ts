05dea6dd0ab25bff7bfb626b6b49fd85febc3b88
Janco <janco.spies@gmail.com>
2023-06-10 09:57:44 +0200

    Fixed dependencies

--- backend/src/signup/signup.service.spec.ts
+++ backend/src/signup/signup.service.spec.ts
@@ -3,14 +3,32 @@ import {
   TestingModule,
 } from '@nestjs/testing';
 import { SignupService } from './signup.service';
+import { AuthService } from '../auth/auth.service';
+import { UsersService } from '../users/users.service';
 
 describe('SignupService', () => {
   let service: SignupService;
+  let authService: AuthService;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule =
       await Test.createTestingModule({
-        providers: [SignupService],
+        providers: [
+          SignupService,
+          {
+            provide: AuthService,
+            useValue: {
+              generateToken: jest.fn(),
+            },
+          },
+          {
+            provide: UsersService,
+            useValue: {
+              findOne: jest.fn(),
+            },
+          },
+        ],
       }).compile();
 
     service = module.get<SignupService>(
