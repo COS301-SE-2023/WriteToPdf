3093bb6e48b648bb6c55d4dd803b77acece2ed6b
Janco <janco.spies@gmail.com>
2023-06-10 09:39:23 +0200

    Implemented basic signup

--- backend/src/signup/signup.module.ts
+++ backend/src/signup/signup.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SignupService } from './signup.service';
+import { UsersService } from '../users/users.service';
+import { AuthService } from '../auth/auth.service';
 
 @Module({
-  providers: [SignupService],
+  providers: [
+    SignupService,
+    UsersService,
+    AuthService,
+  ],
+  exports: [SignupService],
 })
 export class SignupModule {}
--- backend/src/signup/signup.service.ts
+++ backend/src/signup/signup.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { AuthService } from '../auth/auth.service';
+import { UsersService } from '../users/users.service';
 @Injectable()
-export class SignupService {}
+export class SignupService {
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService,
+  ) {}
+
+  async signup(
+    username: string,
+    password: string,
+  ): Promise<any> {
+    const user = await this.usersService.findOne(
+      username,
+    );
+    if (user !== undefined) {
+      throw new HttpException(
+        {
+          status: HttpStatus.CONFLICT,
+          error: 'User already exists',
+        },
+        HttpStatus.CONFLICT,
+      );
+    }
+    //TODO add user to database
+    return this.authService.generateToken(
+      username,
+      password,
+    );
+  }
+}
