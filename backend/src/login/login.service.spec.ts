14bd913c142d03efbf6dbac1807babdb5ef1d4fb
Janco <janco.spies@gmail.com>
2023-06-10 09:25:17 +0200

    Added login service tests

--- backend/src/login/login.service.spec.ts
+++ backend/src/login/login.service.spec.ts
@@ -2,31 +2,115 @@ import {
   Test,
   TestingModule,
 } from '@nestjs/testing';
-// import { UsersService } from '../users/users.service';
-// import { AuthService } from '../auth/auth.service';
 import { LoginService } from './login.service';
-import { AuthModule } from '../auth/auth.module';
-import { UsersModule } from '../users/users.module';
+import { AuthService } from '../auth/auth.service';
+import { UsersService } from '../users/users.service';
+import {
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 
 describe('LoginService', () => {
   let service: LoginService;
+  let authService: AuthService;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule =
       await Test.createTestingModule({
-        imports: [AuthModule, UsersModule],
         providers: [
           LoginService,
-          // UsersService,
-          // AuthService,
+          {
+            provide: AuthService,
+            useValue: {
+              generateToken: jest.fn(),
+            },
+          },
+          {
+            provide: UsersService,
+            useValue: {
+              findOne: jest.fn(),
+            },
+          },
         ],
       }).compile();
 
     service =
       module.get<LoginService>(LoginService);
+    authService =
+      module.get<AuthService>(AuthService);
+    usersService =
+      module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('login', () => {
+    it('should throw exception if user is not found', async () => {
+      const username = 'test';
+      const pass = 'pass';
+      jest
+        .spyOn(usersService, 'findOne')
+        .mockResolvedValue(undefined);
+
+      try {
+        await service.login(username, pass);
+        expect(true).toBe(false);
+      } catch (e) {
+        expect(e).toBeInstanceOf(HttpException);
+        expect(e.getStatus()).toBe(
+          HttpStatus.UNAUTHORIZED,
+        );
+        expect(e.getResponse()).toEqual({
+          status: HttpStatus.UNAUTHORIZED,
+          error: 'User not found',
+        });
+      }
+    });
+
+    it('should throw exception if password is incorrect', async () => {
+      const username = 'test';
+      const pass = 'pass';
+      jest
+        .spyOn(usersService, 'findOne')
+        .mockResolvedValue({
+          username,
+          password: 'wrongpass',
+        });
+
+      try {
+        await service.login(username, pass);
+        expect(true).toBe(false);
+      } catch (e) {
+        expect(e).toBeInstanceOf(HttpException);
+        expect(e.getStatus()).toBe(
+          HttpStatus.UNAUTHORIZED,
+        );
+        expect(e.getResponse()).toEqual({
+          status: HttpStatus.UNAUTHORIZED,
+          error: 'Invalid password',
+        });
+      }
+    });
+
+    it('should return token if credentials are correct', async () => {
+      const username = 'test';
+      const pass = 'pass';
+      const token = 'token';
+      jest
+        .spyOn(usersService, 'findOne')
+        .mockResolvedValue({
+          username,
+          password: pass,
+        });
+      jest
+        .spyOn(authService, 'generateToken')
+        .mockReturnValue(Promise.resolve(token));
+
+      const result = await service.login(
+        username,
+        pass,
+      );
+
+      expect(result).toBe(token);
+    });
   });
 });
