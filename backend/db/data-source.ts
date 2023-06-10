f15139be0e80344f965d53ec2225d1501b779e36
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-06-10 09:59:26 +0200

    Updated entities import line

--- backend/db/data-source.ts
+++ backend/db/data-source.ts
@@ -13,7 +13,7 @@ export const dataSourceOptions: DataSourceOptions =
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    entities: ['dist/**/*entity.js'],
+    entities: ['dist/**/*.entity.js'],
     synchronize: true, // Set this to false in production to prevent automatic schema sync
   };
 
