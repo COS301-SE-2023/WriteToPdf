8ce2d434087a2e1e6bb7a0aed408379732d77157
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-06-11 12:16:54 +0200

    Created db connection for tests

---
+++ backend/src/test-utils/testingModule.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
+import 'dotenv/config'
+
+export const testingModule = () => [
+  TypeOrmModule.forRoot({
+    type: process.env.DB_TYPE as any,
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: ['../../dist/**/*entity.js'],
+    synchronize: true, // Set this to false in production to prevent automatic schema sync
+  }),
+  TypeOrmModule.forFeature([User]),
+];
