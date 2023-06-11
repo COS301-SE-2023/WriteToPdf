9291bf21ef39167e6608790e320012a50b991919
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-06-11 12:15:59 +0200

    Migrated login tests to this file

--- backend/src/users/users.controller.spec.ts
+++ backend/src/users/users.controller.spec.ts
@@ -7,6 +7,13 @@ import { UsersService } from './users.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { testingModule } from '../test-utils/testingModule';
+import { CreateUserDto } from './dto/create-user.dto';
+import {
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { LoginUserDTO } from './dto/login-user.dto';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -14,11 +21,12 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule =
       await Test.createTestingModule({
+        imports: [...testingModule()],
         controllers: [UsersController],
         providers: [
           UsersService,
           {
-            provide: getRepositoryToken(User), // import { getRepositoryToken } from '@nestjs/typeorm';
+            provide: getRepositoryToken(User),
             useClass: Repository,
           },
         ],
@@ -29,7 +37,94 @@ describe('UsersController', () => {
     );
   });
 
+  afterAll(() => {
+    // close DB connection
+  });
+
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return an array of users', async () => {
+    const result = [
+      {
+        UserID: 1,
+        FirstName: 'Test',
+        LastName: 'Test',
+        Email: 'test',
+        Password: 'test',
+      },
+    ];
+    jest
+      .spyOn(controller, 'findAll')
+      .mockImplementation(async () => result);
+
+    expect(await controller.findAll()).toBe(
+      result,
+    );
+  });
+
+  describe('login', () => {
+    it('should be decorated with @Public', () => {
+      const isPublic = Reflect.getMetadata(
+        'isPublic',
+        controller.login,
+      );
+      expect(isPublic).toBe(true);
+    });
+
+    it('should return a user', async () => {
+      const loginUserDto = new LoginUserDTO();
+      loginUserDto.Email = 'test';
+      loginUserDto.Password = 'test';
+
+      const expectedResult = {
+        UserID: 1,
+        FirstName: 'test',
+        LastName: 'test',
+        Email: 'test',
+        Password: 'test',
+      };
+
+      const request = { method: 'POST' };
+
+      jest
+        .spyOn(controller, 'login')
+        .mockImplementation(
+          async () => expectedResult,
+        );
+
+      expect(
+        await controller.login(
+          loginUserDto,
+          request as any,
+        ),
+      ).toBe(expectedResult);
+    });
+
+    it('should throw exception if request method is not POST', async () => {
+      const request = { method: 'GET' };
+      const loginUserDto = new LoginUserDTO();
+      loginUserDto.Email = 'test';
+      loginUserDto.Password = 'test';
+
+      try {
+        await controller.login(
+          loginUserDto,
+          request as any,
+        );
+        expect(true).toBe(false);
+      } catch (error) {
+        expect(error).toBeInstanceOf(
+          HttpException,
+        );
+        expect(error.message).toBe(
+          'Method Not Allowed',
+        );
+        expect(error.status).toBe(
+          HttpStatus.METHOD_NOT_ALLOWED,
+        );
+      }
+    });
   });
 });
