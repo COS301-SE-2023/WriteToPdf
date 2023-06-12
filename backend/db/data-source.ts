2f1dd531bba56451d93212863baa26deae406d2b
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-06-12 13:59:03 +0200

    Added dataSource configurations for testDB

--- backend/db/data-source.ts
+++ backend/db/data-source.ts
@@ -17,7 +17,26 @@ export const dataSourceOptions: DataSourceOptions =
     synchronize: true, // Set this to false in production to prevent automatic schema sync
   };
 
+export const testDBOptions: DataSourceOptions =
+  {
+    type: process.env.DB_TYPE as any,
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.TEST_DB_NAME,
+    entities: ['dist/**/*.entity.js'],
+    synchronize: true, // Set this to false in production to prevent automatic schema sync
+  };
+
 const dataSource = new DataSource(
   dataSourceOptions,
 );
+
+const testDataSource = new DataSource(
+  testDBOptions,
+);
+
+// export { dataSource, testDataSource};
+
 export default dataSource;
