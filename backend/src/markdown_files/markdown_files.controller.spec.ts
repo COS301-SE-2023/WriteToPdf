0d8ad31e33de57945ba977baea99da255f08531d
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-06-13 14:12:28 +0200

    Extended test coverage of markdown file CRUD operations

--- backend/src/markdown_files/markdown_files.controller.spec.ts
+++ backend/src/markdown_files/markdown_files.controller.spec.ts
@@ -9,6 +9,7 @@ import { MarkdownFile } from './entities/markdown_file.entity';
 import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { AuthService } from '../auth/auth.service';
+import { CreateMarkdownFileDTO } from './dto/create-markdown_file.dto';
 
 describe('MarkdownFilesController', () => {
   let controller: MarkdownFilesController;
@@ -54,4 +55,36 @@ describe('MarkdownFilesController', () => {
         expect(result).toEqual(expectedMessage);
     });
   });
+
+  describe('findAll', () => {
+    it('should return an array of markdownFiles', async () => {
+        const expectedMessage = `This action returns all markdownFiles`;
+        const result = controller.findAll();
+        expect(result).toEqual(expectedMessage);
+    });
+  });
+
+  describe('create', () => {
+    it('should create a new markdownFile', async () => {
+        const createMarkdownFileDTO =  new CreateMarkdownFileDTO();
+        createMarkdownFileDTO.Name = "test.md";
+        createMarkdownFileDTO.ID = 123;
+        const expectedMessage = "This action adds a new markdownFile";
+        const result = controller.create(createMarkdownFileDTO);
+        expect(result).toEqual(expectedMessage);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a markdownFile', async () => {
+        const id = 123; // Provide a valid ID for testing
+        const updateMarkdownFileDTO =  new CreateMarkdownFileDTO();
+        updateMarkdownFileDTO.Name = "test.md";
+        updateMarkdownFileDTO.ID = 123;
+        const expectedMessage = `This action updates md file with id: #${id}`;
+        const result = controller.update(id, updateMarkdownFileDTO);
+        expect(result).toEqual(expectedMessage);
+    });
+  });
+
 });
