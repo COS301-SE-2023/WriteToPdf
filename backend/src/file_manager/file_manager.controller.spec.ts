66c8e2c815a8cbf44b7ce910e1966e17161bc7ba
Janco <janco.spies@gmail.com>
2023-06-17 19:58:03 +0200

    Fixed missing dependency

--- backend/src/file_manager/file_manager.controller.spec.ts
+++ backend/src/file_manager/file_manager.controller.spec.ts
@@ -3,6 +3,7 @@ import {
   TestingModule,
 } from '@nestjs/testing';
 import { FileManagerController } from './file_manager.controller';
+import { FileManagerService } from './file_manager.service';
 import { MarkdownFileDTO } from '../markdown_files/dto/markdown_file.dto';
 import {
   HttpException,
@@ -11,11 +12,22 @@ import {
 
 describe('FileManagerController', () => {
   let controller: FileManagerController;
+  let fileManagerService: FileManagerService;
 
   beforeEach(async () => {
     const module: TestingModule =
       await Test.createTestingModule({
         controllers: [FileManagerController],
+        providers: [
+          FileManagerService,
+          {
+            provide: 'FileManagerService',
+            useValue: {
+              create: jest.fn(),
+              rename: jest.fn(),
+            },
+          },
+        ],
       }).compile();
 
     controller =
@@ -29,6 +41,32 @@ describe('FileManagerController', () => {
   });
 
   describe('create', () => {
+    it('should throw exception if request method is not POST', async () => {
+      const request = { method: 'GET' };
+      const markdownFileDTO =
+        new MarkdownFileDTO();
+
+      try {
+        await controller.create(
+          markdownFileDTO,
+          request as any,
+        );
+        expect(true).toBe(false);
+      } catch (error) {
+        expect(error).toBeInstanceOf(
+          HttpException,
+        );
+        expect(error.message).toBe(
+          'Method Not Allowed',
+        );
+        expect(error.status).toBe(
+          HttpStatus.METHOD_NOT_ALLOWED,
+        );
+      }
+    });
+  });
+
+  describe('rename', () => {
     it('should throw exception if request method is not POST', async () => {
       const request = { method: 'GET' };
       const markdownFileDTO =
