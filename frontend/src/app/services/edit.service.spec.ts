bd4c15627f9b6eaa6143902aef66a6d9cb78ee89
JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>
2023-06-19 17:33:11 +0200

    Created edit service

---
+++ frontend/src/app/services/edit.service.spec.ts
@@ -0,0 +1,18 @@
+import 'zone.js/testing';
+import { TestBed } from '@angular/core/testing';
+import { expect } from '@jest/globals';
+
+import { EditService } from './edit.service';
+
+describe('EditService', () => {
+  let service: EditService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(EditService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
