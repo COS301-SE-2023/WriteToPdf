19293d45dcc48b08cd0beeca80436cdb4fd9c049
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-06-21 15:15:04 +0200

    Got file manager service to be defined

--- backend/src/file_manager/file_manager.service.spec.ts
+++ backend/src/file_manager/file_manager.service.spec.ts
@@ -5,26 +5,58 @@ import {
 import { FileManagerService } from './file_manager.service';
 import { MarkdownFilesService } from '../markdown_files/markdown_files.service';
 import { FoldersService } from '../folders/folders.service';
+import { Repository } from 'typeorm';
+import {
+  TypeOrmModule,
+  getRepositoryToken,
+} from '@nestjs/typeorm';
+import { MarkdownFile } from '../markdown_files/entities/markdown_file.entity';
+import { testingModule } from '../test-utils/testingModule';
+import { Folder } from '../folders/entities/folder.entity';
 
 describe('FileManagerService', () => {
-  let service: FileManagerService;
+  let fileManagerService: FileManagerService;
+  let markdownFilesService: MarkdownFilesService;
+  let foldersService: FoldersService;
 
   beforeEach(async () => {
     const module: TestingModule =
       await Test.createTestingModule({
+        imports: [...testingModule()],
         providers: [
           FileManagerService,
           MarkdownFilesService,
           FoldersService,
+          {
+            provide:
+              getRepositoryToken(MarkdownFile),
+            useClass: Repository,
+          },
+          {
+            provide: getRepositoryToken(Folder),
+            useClass: Repository,
+          },
         ],
       }).compile();
 
-    service = module.get<FileManagerService>(
-      FileManagerService,
+    fileManagerService =
+      module.get<FileManagerService>(
+        FileManagerService,
+      );
+    markdownFilesService =
+      module.get<MarkdownFilesService>(
+        MarkdownFilesService,
+      );
+    foldersService = module.get<FoldersService>(
+      FoldersService,
     );
+
+    module.close();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('root/config', () => {
+    it('file manager service should be defined', () => {
+      expect(fileManagerService).toBeDefined();
+    });
   });
 });
