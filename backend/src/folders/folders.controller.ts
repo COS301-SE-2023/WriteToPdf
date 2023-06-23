cac0a955649e68ffa8f09ec4aaa7b47b77f16794
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-06-23 10:16:21 +0200

    Removed stubbed CRUD calls

--- backend/src/folders/folders.controller.ts
+++ backend/src/folders/folders.controller.ts
@@ -1,11 +1,11 @@
 import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
+    Controller,
+    Get,
+    Post,
+    Body,
+    Patch,
+    Param,
+    Delete,
 } from '@nestjs/common';
 import { FoldersService } from './folders.service';
 import { CreateFolderDTO } from './dto/create-folder.dto';
@@ -13,42 +13,9 @@ import { UpdateFolderDTO } from './dto/update-folder.dto';
 
 @Controller('folders')
 export class FoldersController {
-  constructor(
-    private readonly foldersService: FoldersService,
-  ) {}
+    constructor(
+        private readonly foldersService: FoldersService,
+    ) { }
 
-  @Post()
-  create(
-    @Body() createFolderDTO: CreateFolderDTO,
-  ) {
-    return this.foldersService.create(
-      createFolderDTO,
-    );
-  }
-
-  @Get()
-  findAll() {
-    return this.foldersService.findAll();
-  }
-
-  @Get(':FolderID')
-  findOne(@Param('FolderID') FolderID: string) {
-    return this.foldersService.findOne(FolderID);
-  }
-
-  @Patch(':FolderID')
-  update(
-    @Param('FolderID') FolderID: string,
-    @Body() updateFolderDTO: UpdateFolderDTO,
-  ) {
-    return this.foldersService.update(
-      FolderID,
-      updateFolderDTO,
-    );
-  }
-
-  @Delete(':FolderID')
-  remove(@Param('FolderID') FolderID: string) {
-    return this.foldersService.remove(FolderID);
-  }
+    // CRUD calls to foldersService go here 
 }
