2925de0c2283553aa69b6ea85c79b76fdbf1147c
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-06-23 10:14:40 +0200

    Stubbed CRUD operations removed

--- backend/src/assets/assets.service.ts
+++ backend/src/assets/assets.service.ts
@@ -4,26 +4,5 @@ import { UpdateAssetDTO } from './dto/update-asset.dto';
 
 @Injectable()
 export class AssetsService {
-  create(createAssetDTO: CreateAssetDTO) {
-    return 'This action adds a new asset';
-  }
-
-  findAll() {
-    return `This action returns all assets`;
-  }
-
-  findOne(AssetID: string) {
-    return `This action returns a #${AssetID} asset`;
-  }
-
-  update(
-    AssetID: string,
-    updateAssetDTO: UpdateAssetDTO,
-  ) {
-    return `This action updates a #${AssetID} asset`;
-  }
-
-  remove(AssetID: string) {
-    return `This action removes a #${AssetID} asset`;
-  }
+  // CRUD Operations go here
 }
