df5361b722ba9669e5b2c57092a8d1d74b0a6055
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-06-23 10:31:14 +0200

    Removed stubbed functions

--- backend/src/assets/assets.controller.ts
+++ backend/src/assets/assets.controller.ts
@@ -16,37 +16,4 @@ export class AssetsController {
   constructor(
     private readonly assetsService: AssetsService,
   ) {}
-
-  @Post()
-  create(@Body() createAssetDTO: CreateAssetDTO) {
-    return this.assetsService.create(
-      createAssetDTO,
-    );
-  }
-
-  @Get()
-  findAll() {
-    return this.assetsService.findAll();
-  }
-
-  @Get(':AssetID')
-  findOne(@Param('AssetID') AssetID: string) {
-    return this.assetsService.findOne(AssetID);
-  }
-
-  @Patch(':AssetID')
-  update(
-    @Param('AssetID') AssetID: string,
-    @Body() updateAssetDTO: UpdateAssetDTO,
-  ) {
-    return this.assetsService.update(
-      AssetID,
-      updateAssetDTO,
-    );
-  }
-
-  @Delete(':AssetID')
-  remove(@Param('AssetID') AssetID: string) {
-    return this.assetsService.remove(AssetID);
-  }
 }
