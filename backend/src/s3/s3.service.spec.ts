6ea9729bc7251eb711cd78fd2db48911238a5248
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-06-24 19:49:51 +0200

    Updated formatting (npm run format)

--- backend/src/s3/s3.service.spec.ts
+++ backend/src/s3/s3.service.spec.ts
@@ -1,13 +1,17 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import {
+  Test,
+  TestingModule,
+} from '@nestjs/testing';
 import { S3Service } from './s3.service';
 
 describe('S3Service', () => {
   let service: S3Service;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [S3Service],
-    }).compile();
+    const module: TestingModule =
+      await Test.createTestingModule({
+        providers: [S3Service],
+      }).compile();
 
     service = module.get<S3Service>(S3Service);
   });
