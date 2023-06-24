b208db4ebc760397a5a6e97bf0d59f657115aab2
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-06-24 19:51:24 +0200

    Removed outdated imports

--- backend/src/assets/assets.controller.ts
+++ backend/src/assets/assets.controller.ts
@@ -1,15 +1,5 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AssetsService } from './assets.service';
-import { CreateAssetDTO } from './dto/create-asset.dto';
-import { UpdateAssetDTO } from './dto/update-asset.dto';
 
 @Controller('assets')
 export class AssetsController {
--- backend/src/assets/assets.service.ts
+++ backend/src/assets/assets.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAssetDTO } from './dto/create-asset.dto';
-import { UpdateAssetDTO } from './dto/update-asset.dto';
 
 @Injectable()
 export class AssetsService {
