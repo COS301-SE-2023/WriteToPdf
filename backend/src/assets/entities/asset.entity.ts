2fc543e641ba625c59a4b3da9612a18a30381d89
Janco <janco.spies@gmail.com>
2023-06-25 19:57:36 +0200

    Updated asset attributes

--- backend/src/assets/entities/asset.entity.ts
+++ backend/src/assets/entities/asset.entity.ts
@@ -1,13 +1,12 @@
 import {
   Entity,
   Column,
-  PrimaryGeneratedColumn,
-  JoinColumn,
+  PrimaryColumn,
 } from 'typeorm';
 
 @Entity('ASSETS')
 export class Asset {
-  @PrimaryGeneratedColumn()
+  @PrimaryColumn()
   AssetID: string;
 
   @Column()
@@ -35,6 +34,9 @@ export class Asset {
   })
   Size: number;
 
-  @JoinColumn()
+  @Column()
   ParentFolderID: string;
+
+  @Column()
+  UserID: number;
 }
