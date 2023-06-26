737d390bc7910fa1b365d6e5ff6667e7c9af3af7
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-06-26 14:47:39 +0200

    Ran formatter

--- backend/src/conversion/conversion.service.spec.ts
+++ backend/src/conversion/conversion.service.spec.ts
@@ -1,15 +1,21 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import {
+  Test,
+  TestingModule,
+} from '@nestjs/testing';
 import { ConversionService } from './conversion.service';
 
 describe('ConversionService', () => {
   let service: ConversionService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ConversionService],
-    }).compile();
+    const module: TestingModule =
+      await Test.createTestingModule({
+        providers: [ConversionService],
+      }).compile();
 
-    service = module.get<ConversionService>(ConversionService);
+    service = module.get<ConversionService>(
+      ConversionService,
+    );
   });
 
   it('should be defined', () => {
