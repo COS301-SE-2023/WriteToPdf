782b2f6ed7abe08a0afe75bbb3944006a9fe5780
Dylan Kapnias <u18108467@tuks.co.za>
2023-07-03 16:16:06 +0200

    Frontend deployed with amplify

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { InfrastructureStack } from "../lib/infrastructure-stack";
+import { WriteToPDFStack } from "../lib/infrastructure-stack";
 
 const app = new cdk.App();
-new InfrastructureStack(app, "InfrastructureStack", {
+new WriteToPDFStack(app, "WriteToPDFStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new InfrastructureStack(app, "InfrastructureStack", {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { region: "af-south-1" },
+  env: { region: "us-east-1" },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
