ea24b9dbfb93500687749958a8cb87644977ab8c
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-07-12 18:24:52 +0200

    Created upload service function
    Co-authored-by: JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>

--- backend/src/image_manager/image_manager.service.ts
+++ backend/src/image_manager/image_manager.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
-import { CreateImageManagerDto } from './dto/create-image_manager.dto';
-import { UpdateImageManagerDto } from './dto/update-image_manager.dto';
+import { ImageDTO } from './dto/image.dto';
+import { S3Service } from '../s3/s3.service';
 
 @Injectable()
 export class ImageManagerService {
-  create(
-    createImageManagerDto: CreateImageManagerDto,
-  ) {
+  constructor(
+    private readonly s3Service: S3Service,
+  ) {}
+
+  create(createImageDto: ImageDTO) {
     return 'This action adds a new imageManager';
   }
 
@@ -18,14 +20,19 @@ export class ImageManagerService {
     return `This action returns a #${id} imageManager`;
   }
 
-  update(
-    id: number,
-    updateImageManagerDto: UpdateImageManagerDto,
-  ) {
+  update(id: number, updateImageDto: ImageDTO) {
     return `This action updates a #${id} imageManager`;
   }
 
   remove(id: number) {
     return `This action removes a #${id} imageManager`;
+  }
+
+  upload(
+    uploadImageDto: ImageDTO,
+  ): Promise<ImageDTO> {
+    return this.s3Service.saveImage(
+      uploadImageDto,
+    );
   }
 }
