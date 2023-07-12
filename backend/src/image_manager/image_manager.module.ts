2521fa51c969c1371e8df9ef0bfabe449733c449
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-07-12 18:24:25 +0200

    Imported necessary dependencies
    Co-authored-by: JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>

--- backend/src/image_manager/image_manager.module.ts
+++ backend/src/image_manager/image_manager.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ImageManagerService } from './image_manager.service';
 import { ImageManagerController } from './image_manager.controller';
+import { S3Service } from '../s3/s3.service';
 
 @Module({
   controllers: [ImageManagerController],
-  providers: [ImageManagerService],
+  providers: [ImageManagerService, S3Service],
 })
 export class ImageManagerModule {}
