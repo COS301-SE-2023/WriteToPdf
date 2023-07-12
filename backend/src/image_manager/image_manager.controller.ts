428783711927a6a03474fbcf41438ea1907e49a4
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-07-12 18:23:58 +0200

    Created controller endpoint for upload image Co-authored-by: JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>

--- backend/src/image_manager/image_manager.controller.ts
+++ backend/src/image_manager/image_manager.controller.ts
@@ -8,19 +8,28 @@ import {
   Delete,
 } from '@nestjs/common';
 import { ImageManagerService } from './image_manager.service';
-import { CreateImageManagerDto } from './dto/create-image_manager.dto';
-import { UpdateImageManagerDto } from './dto/update-image_manager.dto';
+import { ImageDTO } from './dto/image.dto';
 
-@Controller('image-manager')
+@Controller('image_manager')
 export class ImageManagerController {
   constructor(
     private readonly imageManagerService: ImageManagerService,
   ) {}
 
+  @Post('upload')
+  upload(
+    @Body()
+    uploadImageDto: ImageDTO,
+  ) {
+    return this.imageManagerService.upload(
+      uploadImageDto,
+    );
+  }
+
   @Post()
   create(
     @Body()
-    createImageManagerDto: CreateImageManagerDto,
+    createImageManagerDto: ImageDTO,
   ) {
     return this.imageManagerService.create(
       createImageManagerDto,
@@ -41,7 +50,7 @@ export class ImageManagerController {
   update(
     @Param('id') id: string,
     @Body()
-    updateImageManagerDto: UpdateImageManagerDto,
+    updateImageManagerDto: ImageDTO,
   ) {
     return this.imageManagerService.update(
       +id,
