28bb5cb0536b3ba0e481c7776671fe6c2db41f31
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-07-12 16:48:08 +0200

    Deleted redundant dto

--- backend/src/image_manager/dto/update-image_manager.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateImageManagerDto } from './create-image_manager.dto';
-
-export class UpdateImageManagerDto extends PartialType(
-  CreateImageManagerDto,
-) {}
