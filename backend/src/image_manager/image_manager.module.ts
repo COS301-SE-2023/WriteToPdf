25f058ffd246a58a87d6b0316cbc3990b7dc23c4
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-07-15 15:49:24 +0200

    Solved dependency issues

--- backend/src/image_manager/image_manager.module.ts
+++ backend/src/image_manager/image_manager.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { ImageManagerService } from './image_manager.service';
 import { ImageManagerController } from './image_manager.controller';
 import { S3Service } from '../s3/s3.service';
+import { AssetsService } from '../assets/assets.service';
+import { Asset } from '../assets/entities/asset.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthService } from '../auth/auth.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Asset])],
   controllers: [ImageManagerController],
-  providers: [ImageManagerService, S3Service],
+  providers: [
+    ImageManagerService,
+    S3Service,
+    AssetsService,
+    AuthService,
+  ],
 })
 export class ImageManagerModule {}
