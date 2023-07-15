7741d1fcb29447df64497e2f05c4cb4cdde04b52
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-07-15 15:35:08 +0200

    Created AssetDTO

--- backend/src/assets/dto/asset.dto.ts
+++ backend/src/assets/dto/asset.dto.ts
@@ -1,1 +1,23 @@
-export class AssetDTO {}
+export class AssetDTO {
+  AssetID: string;
+  Format: string;
+  FileName: string;
+  ConvertedElement: string;
+  Image: string;
+  DateCreated: Date;
+  Size: number;
+  ParentFolderID: string;
+  UserID: number;
+
+  constructor() {
+    this.AssetID = undefined;
+    this.Format = undefined;
+    this.FileName = undefined;
+    this.ConvertedElement = undefined;
+    this.Image = undefined;
+    this.DateCreated = undefined;
+    this.Size = undefined;
+    this.ParentFolderID = undefined;
+    this.UserID = undefined;
+  }
+}
