88051b0592b6ba4fdf90dfeb5c3d7df29b0f1e80
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-07-15 15:33:46 +0200

    Implemented createImage

--- backend/src/assets/assets.service.ts
+++ backend/src/assets/assets.service.ts
@@ -1,6 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { Asset } from './entities/asset.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthService } from '../auth/auth.service';
+import { ImageDTO } from 'src/image_manager/dto/image.dto';
+import { AssetDTO } from './dto/asset.dto';
 
 @Injectable()
 export class AssetsService {
-  // CRUD Operations go here
+  constructor(
+    @InjectRepository(Asset)
+    private assetsRepository: Repository<Asset>,
+    private authService: AuthService,
+  ) {}
+
+  // findRecent(): Promise<Asset[]> {
+  //   const response = this.assetsRepository.find(); // SELECT * FROM assets;
+  //   response = response.orderby('DateCreated', 'DESC');
+  //   return response;
+  // }
+
+  createImage(
+    uploadAssetDto: ImageDTO,
+  ): Promise<Asset> {
+    const newAssetDto = new AssetDTO();
+    newAssetDto.AssetID = '123';
+    newAssetDto.Format = 'image';
+    newAssetDto.FileName =
+      uploadAssetDto.FileName;
+    newAssetDto.ConvertedElement = 'null';
+    newAssetDto.Image = 'null';
+    newAssetDto.DateCreated = new Date();
+    newAssetDto.Size = uploadAssetDto.Size;
+    newAssetDto.ParentFolderID = '0';
+    newAssetDto.UserID = 6;
+    const newAsset =
+      this.assetsRepository.create(newAssetDto);
+    return this.assetsRepository.save(newAsset);
+  }
+
+  retrieveAll(): Promise<Asset[]> {
+    return this.assetsRepository.find(); // SELECT * FROM assets;
+  }
 }
