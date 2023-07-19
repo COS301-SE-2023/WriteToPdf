ec88fbf0a4a7ef2e0692bf70060e6e24c2fa48c1
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-07-19 20:29:24 +0200

    Deleted unused files

--- backend/src/text_manager/text_manager.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TextManagerService } from './text_manager.service';
-import { TextManagerController } from './text_manager.controller';
-
-@Module({
-  controllers: [TextManagerController],
-  providers: [TextManagerService],
-})
-export class TextManagerModule {}
