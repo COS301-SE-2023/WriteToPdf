f1f61241d60c059ff77a5a6fb4f6ae2a23798584
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-07-20 11:29:04 +0200

    Deleted unused dtos

--- backend/src/asset_manager/dto/update-asset_manager.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAssetManagerDto } from './create-asset_manager.dto';
-
-export class UpdateAssetManagerDto extends PartialType(
-  CreateAssetManagerDto,
-) {}
