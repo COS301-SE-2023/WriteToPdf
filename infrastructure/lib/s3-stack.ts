d0965fea001d4da32424aa0804e54c3744cf46c5
Dylan Kapnias <u18108467@tuks.co.za>
2023-07-23 14:45:48 +0200

    Added sns and sqs to CDK

--- infrastructure/lib/s3-stack.ts
+++ infrastructure/lib/s3-stack.ts
@@ -1,10 +1,21 @@
 import { App, CfnOutput, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
-import { PolicyStatement, ServicePrincipal, User } from "aws-cdk-lib/aws-iam";
+import {
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+  User,
+} from "aws-cdk-lib/aws-iam";
 import { Bucket, BucketAccessControl } from "aws-cdk-lib/aws-s3";
+import { Topic } from "aws-cdk-lib/aws-sns";
+import { Alias } from "aws-cdk-lib/aws-kms";
+import { SqsSubscription } from "aws-cdk-lib/aws-sns-subscriptions";
+import { Queue } from "aws-cdk-lib/aws-sqs";
 
 export class S3Stack extends Stack {
   constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    let textract = new ServicePrincipal("textract.amazonaws.com");
 
     // Create S3 bucket to hold user assets
     const bucket = new Bucket(this, "WriteToPDFAppBucket", {
@@ -13,9 +24,19 @@ export class S3Stack extends Stack {
       // blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
       accessControl: BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
     });
-
-    let textract = new ServicePrincipal("textract.amazonaws.com");
     bucket.grantReadWrite(textract);
+
+    let queue = new Queue(this, "writetopdf-textract-queue", {});
+
+    let topic = new Topic(this, "writetopdf-textract-topic", {
+      masterKey: Alias.fromAliasName(this, "defaultKey", "alias/aws/sns"),
+    });
+    topic.addSubscription(new SqsSubscription(queue));
+
+    let role = new Role(this, "writetopdf-textract-role", {
+      assumedBy: textract,
+    });
+    topic.grantPublish(role);
 
     const user = new User(this, "MyIAMUser", {
       userName: "writetopdf-webapp-test",
@@ -35,5 +56,11 @@ export class S3Stack extends Stack {
     new CfnOutput(this, "S3BucketRegion", {
       value: bucket.bucketRegionalDomainName,
     });
+
+    new CfnOutput(this, "TopicArn", { value: topic.topicArn });
+
+    new CfnOutput(this, "RoleArn", { value: role.roleArn });
+
+    new CfnOutput(this, "QueueUrl", { value: queue.queueUrl });
   }
 }
