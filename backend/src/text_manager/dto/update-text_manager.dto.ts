23a016debd29a962909c2de5baa61768a11a77e4
Janco <janco.spies@gmail.com>
2023-07-24 17:58:33 +0200

    Remove auto generated files

--- backend/src/text_manager/dto/update-text_manager.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTextManagerDto } from './create-text_manager.dto';
-
-export class UpdateTextManagerDto extends PartialType(
-  CreateTextManagerDto,
-) {}
