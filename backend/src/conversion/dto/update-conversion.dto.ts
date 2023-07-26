80565457a6779c1957e470793c2450b862934007
Janco <janco.spies@gmail.com>
2023-07-26 13:59:25 +0200

    Removed unused DTOs

--- backend/src/conversion/dto/update-conversion.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateConversionDto } from './create-conversion.dto';
-
-export class UpdateConversionDto extends PartialType(
-  CreateConversionDto,
-) {}
