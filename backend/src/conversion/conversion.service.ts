77e68bdf7edec939be50866394097b804b10aeef
Janco <janco.spies@gmail.com>
2023-07-27 16:01:00 +0200

    Refactored convertPdfToHtml

--- backend/src/conversion/conversion.service.ts
+++ backend/src/conversion/conversion.service.ts
@@ -82,21 +82,26 @@ export class ConversionService {
   async convertPdfToHtml(
     pdf: Buffer,
   ): Promise<string> {
-    // const tempPdfPath = `./temp_file.pdf`;
-    // await fs.writeFile(
-    //   tempPdfPath,
-    //   pdf,
-    //   'binary',
-    // );
+    let html = '';
+    // Generate a random string to use as a temporary file name
+    const tempPdfPath = `temp-${Math.random().toString(
+      36,
+    )}.pdf`;
+    try {
+      await fs.writeFile(
+        tempPdfPath,
+        pdf,
+        'binary',
+      );
 
-    // // Use the file path as the argument for pdf2html.html()
-    // const html = await pdf2html.html(tempPdfPath);
+      // Use the file path as the argument for pdf2html.html()
+      html = await pdf2html.html(tempPdfPath);
+    } finally {
+      // Delete the temporary file
+      await fs.unlink(tempPdfPath);
+    }
 
-    // // Delete the temporary file
-    // await fs.unlink(tempPdfPath);
-
-    // return html;
-    return '';
+    return html;
   }
 
   convertTxtToHtml(txt: string): string {
