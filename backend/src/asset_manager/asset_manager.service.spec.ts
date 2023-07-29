2851ff9b41f788e8dddc4769addeb602c1f985a5
Janco <janco.spies@gmail.com>
2023-07-29 16:47:00 +0200

    Updated tests

--- backend/src/asset_manager/asset_manager.service.spec.ts
+++ backend/src/asset_manager/asset_manager.service.spec.ts
@@ -103,6 +103,16 @@ describe('AssetManagerService', () => {
       retrieveAllDTO.UserID = 1;
       retrieveAllDTO.ParentFolderID = 'test';
 
+      const imgAsset = new Asset();
+      imgAsset.AssetID = 'test';
+      imgAsset.Format = 'image';
+      imgAsset.UserID = 1;
+
+      const textAsset = new Asset();
+      textAsset.AssetID = 'test';
+      textAsset.Format = 'text';
+      textAsset.UserID = 1;
+
       const imgAssetDTO = new AssetDTO();
       imgAssetDTO.AssetID = 'test';
       imgAssetDTO.Format = 'image';
@@ -113,7 +123,7 @@ describe('AssetManagerService', () => {
       textAssetDTO.Format = 'text';
       textAssetDTO.UserID = 1;
 
-      const assets = [imgAssetDTO, textAssetDTO];
+      const assets = [imgAsset, textAsset];
 
       jest
         .spyOn(assetsService, 'retrieveAllAssets')
@@ -213,9 +223,14 @@ describe('AssetManagerService', () => {
         assetDTO.Format = 'image';
         assetDTO.UserID = 1;
 
+        const asset = new Asset();
+        asset.AssetID = 'test';
+        asset.Format = 'image';
+        asset.UserID = 1;
+
         jest
           .spyOn(assetsService, 'renameAsset')
-          .mockResolvedValue(assetDTO);
+          .mockResolvedValue(asset);
 
         await service.rename_asset(assetDTO);
 
