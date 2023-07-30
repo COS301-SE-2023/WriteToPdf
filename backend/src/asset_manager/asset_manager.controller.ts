39957029a2ed42800c173384408df2bde23bf877
Jake Weatherhead <jakeweatherhead1@gmail.com>
2023-07-30 20:21:59 +0200

    Removed expensive console.log

--- backend/src/asset_manager/asset_manager.controller.ts
+++ backend/src/asset_manager/asset_manager.controller.ts
@@ -37,11 +37,6 @@ export class AssetManagerController {
       );
     }
 
-    console.log(
-      'Image data sent from frontend: ',
-      uploadImageDTO.Image,
-    );
-
     return this.assetManagerService.upload_asset(
       uploadImageDTO,
     );
