d41ac21f5c3c1fb79f8ba8af4e133d4a8955fb4f
Dylan Shane Kapnias <u18108467@tuks.co.za>
2023-07-31 13:04:16 +0200

    Started Textract Tests

--- backend/src/textract/textract.service.spec.ts
+++ backend/src/textract/textract.service.spec.ts
@@ -3,9 +3,28 @@ import {
   TestingModule,
 } from '@nestjs/testing';
 import { TextractService } from './textract.service';
+import { mockClient } from 'aws-sdk-client-mock';
+import {
+  GetDocumentTextDetectionCommand,
+  StartDocumentTextDetectionCommand,
+  TextractClient,
+  Block,
+  JobStatus,
+} from '@aws-sdk/client-textract';
+import {
+  DeleteMessageCommand,
+  Message,
+  ReceiveMessageCommand,
+  SQSClient,
+} from '@aws-sdk/client-sqs';
+import { AssetDTO } from '../assets/dto/asset.dto';
 
 describe('TextractService', () => {
-  let service: TextractService;
+  let textractService: TextractService;
+  const mockTextractClient = mockClient(
+    TextractClient,
+  );
+  const mockSqsClient = mockClient(SQSClient);
 
   beforeEach(async () => {
     const module: TestingModule =
@@ -13,12 +32,61 @@ describe('TextractService', () => {
         providers: [TextractService],
       }).compile();
 
-    service = module.get<TextractService>(
+    textractService = module.get<TextractService>(
       TextractService,
     );
+
+    mockSqsClient.reset();
+    mockTextractClient.reset();
+
+    mockTextractClient
+      .on(StartDocumentTextDetectionCommand)
+      .resolves({ JobId: '1' });
+
+    const blk: Block = { Text: 'Inside Block' };
+    const blkRet: Block[] = [];
+    blkRet.push(blk);
+
+    mockTextractClient
+      .on(GetDocumentTextDetectionCommand)
+      .resolves({ Blocks: blkRet });
+
+    const jsonBody = {
+      Message: `{"Status":"SUCCEEDED"}`,
+    };
+    const msg: Message = {
+      ReceiptHandle: 'Inside Message',
+      Body: JSON.stringify(jsonBody),
+    };
+
+    const msgRet: Message[] = [];
+    msgRet.push(msg);
+
+    mockSqsClient
+      .on(ReceiveMessageCommand)
+      .resolves({ Messages: msgRet });
+    mockSqsClient
+      .on(DeleteMessageCommand)
+      .resolves({});
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(textractService).toBeDefined();
+  });
+
+  describe('Asynchronous extract', () => {
+    it('should return an extraction object', async () => {
+      const assetDTO = new AssetDTO();
+      assetDTO.UserID = 16;
+      assetDTO.AssetID = '1';
+
+      const result =
+        textractService._extractDocumentAsynchronous(
+          assetDTO,
+          'text',
+        );
+
+      expect(result).toBeDefined();
+    });
   });
 });
