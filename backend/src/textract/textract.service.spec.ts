e226f4bdfb44ba6cce792a5d7a7777a1fa0ac94e
Dylan Shane Kapnias <u18108467@tuks.co.za>
2023-07-31 13:04:16 +0200

    Started Textract Tests

--- backend/src/textract/textract.service.spec.ts
+++ backend/src/textract/textract.service.spec.ts
@@ -9,10 +9,7 @@ import {
   StartDocumentTextDetectionCommand,
   TextractClient,
   Block,
-  DetectDocumentTextCommand,
-  AnalyzeDocumentCommand,
-  StartDocumentAnalysisCommand,
-  GetDocumentAnalysisCommand,
+  JobStatus,
 } from '@aws-sdk/client-textract';
 import {
   DeleteMessageCommand,
@@ -41,33 +38,16 @@ describe('TextractService', () => {
 
     mockSqsClient.reset();
     mockTextractClient.reset();
+    mockTextractClient
+      .on(StartDocumentTextDetectionCommand)
+      .resolves({ JobId: '1' });
 
     const blk: Block = { Text: 'Inside Block' };
     const blkRet: Block[] = [];
     blkRet.push(blk);
 
     mockTextractClient
-      .on(StartDocumentTextDetectionCommand)
-      .resolves({ JobId: '1' });
-
-    mockTextractClient
       .on(GetDocumentTextDetectionCommand)
-      .resolves({ Blocks: blkRet });
-
-    mockTextractClient
-      .on(StartDocumentAnalysisCommand)
-      .resolves({ JobId: '1' });
-
-    mockTextractClient
-      .on(GetDocumentAnalysisCommand)
-      .resolves({ Blocks: blkRet });
-
-    mockTextractClient
-      .on(DetectDocumentTextCommand)
-      .resolves({ Blocks: blkRet });
-
-    mockTextractClient
-      .on(AnalyzeDocumentCommand)
       .resolves({ Blocks: blkRet });
 
     const jsonBody = {
@@ -77,6 +57,7 @@ describe('TextractService', () => {
       ReceiptHandle: 'Inside Message',
       Body: JSON.stringify(jsonBody),
     };
+
     const msgRet: Message[] = [];
     msgRet.push(msg);
 
@@ -93,292 +74,18 @@ describe('TextractService', () => {
   });
 
   describe('Asynchronous extract', () => {
-    describe('Text Extraction', () => {
-      it('should return an extraction object', async () => {
-        const assetDTO = new AssetDTO();
-        assetDTO.UserID = 1;
-        assetDTO.AssetID = '1';
+    it('should return an extraction object', async () => {
+      const assetDTO = new AssetDTO();
+      assetDTO.UserID = 16;
+      assetDTO.AssetID = '1';
 
-        const expectedResult = {
-          Name: '1',
-          ExtractType: 'text',
-          Extracted: [{ Text: 'Inside Block' }],
-        };
+      const result =
+        textractService._extractDocumentAsynchronous(
+          assetDTO,
+          'text',
+        );
 
-        const result =
-          await textractService._extractDocumentAsynchronous(
-            assetDTO,
-            'text',
-          );
-
-        expect(result).toBeDefined();
-        expect(result).toEqual(expectedResult);
-      });
-    });
-
-    describe('Table Extraction', () => {
-      it('should return an extraction object', async () => {
-        const assetDTO = new AssetDTO();
-        assetDTO.UserID = 1;
-        assetDTO.AssetID = '1';
-
-        const expectedResult = {
-          Name: '1',
-          ExtractType: 'table',
-          Extracted: [{ Text: 'Inside Block' }],
-        };
-
-        const result =
-          await textractService._extractDocumentAsynchronous(
-            assetDTO,
-            'table',
-          );
-
-        expect(result).toBeDefined();
-        expect(result).toEqual(expectedResult);
-      });
-    });
-  });
-
-  describe('Synchronous extract', () => {
-    describe('Text Extraction', () => {
-      it('should return an extraction object', async () => {
-        const assetDTO = new AssetDTO();
-        assetDTO.UserID = 1;
-        assetDTO.AssetID = '1';
-
-        const expectedResult = {
-          Blocks: [{ Text: 'Inside Block' }],
-        };
-
-        const result =
-          await textractService._extractDocumentSynchronous(
-            assetDTO,
-            'text',
-          );
-
-        expect(result).toBeDefined();
-        expect(result).toEqual(expectedResult);
-      });
-    });
-
-    describe('Table Extraction', () => {
-      it('should return an extraction object', async () => {
-        const assetDTO = new AssetDTO();
-        assetDTO.UserID = 1;
-        assetDTO.AssetID = '1';
-
-        const expectedResult = {
-          Blocks: [{ Text: 'Inside Block' }],
-        };
-
-        const result =
-          await textractService._extractDocumentSynchronous(
-            assetDTO,
-            'table',
-          );
-
-        expect(result).toBeDefined();
-        expect(result).toEqual(expectedResult);
-      });
-    });
-  });
-
-  describe('Normal Endpoint', () => {
-    describe('Synchronous', () => {
-      describe('Text Extraction', () => {
-        it('should return an extraction object', async () => {
-          const assetDTO = new AssetDTO();
-          assetDTO.UserID = 1;
-          assetDTO.AssetID = '1';
-
-          const expectedResult = {
-            Blocks: [{ Text: 'Inside Block' }],
-          };
-
-          const result =
-            await textractService.extractDocument(
-              'sync',
-              assetDTO,
-              'text',
-            );
-
-          expect(result).toBeDefined();
-          expect(result).toEqual(expectedResult);
-        });
-
-        it('should console log with error', async () => {
-          const assetDTO = new AssetDTO();
-          assetDTO.UserID = 1;
-          assetDTO.AssetID = '1';
-
-          mockTextractClient
-            .on(DetectDocumentTextCommand)
-            .rejects(
-              new Error('Force Test Error'),
-            );
-          const mockConsole = jest.spyOn(
-            global.console,
-            'log',
-          );
-
-          await textractService.extractDocument(
-            'sync',
-            assetDTO,
-            'text',
-          );
-
-          expect(mockConsole).toBeCalled();
-          mockConsole.mockRestore();
-        });
-      });
-
-      describe('Table Extraction', () => {
-        it('should return an extraction object', async () => {
-          const assetDTO = new AssetDTO();
-          assetDTO.UserID = 1;
-          assetDTO.AssetID = '1';
-
-          const expectedResult = {
-            Blocks: [{ Text: 'Inside Block' }],
-          };
-
-          const result =
-            await textractService.extractDocument(
-              'sync',
-              assetDTO,
-              'table',
-            );
-
-          expect(result).toBeDefined();
-          expect(result).toEqual(expectedResult);
-        });
-
-        it('should console log with error', async () => {
-          const assetDTO = new AssetDTO();
-          assetDTO.UserID = 1;
-          assetDTO.AssetID = '1';
-
-          mockTextractClient
-            .on(AnalyzeDocumentCommand)
-            .rejects(
-              new Error('Force Test Error'),
-            );
-          const mockConsole = jest.spyOn(
-            global.console,
-            'log',
-          );
-
-          await textractService.extractDocument(
-            'sync',
-            assetDTO,
-            'table',
-          );
-
-          expect(mockConsole).toBeCalled();
-          mockConsole.mockRestore();
-        });
-      });
-    });
-
-    describe('Asynchronous', () => {
-      describe('Text Extraction', () => {
-        it('should return an extraction object', async () => {
-          const assetDTO = new AssetDTO();
-          assetDTO.UserID = 1;
-          assetDTO.AssetID = '1';
-
-          const expectedResult = {
-            Name: '1',
-            ExtractType: 'text',
-            Extracted: [{ Text: 'Inside Block' }],
-          };
-
-          const result =
-            await textractService.extractDocument(
-              'async',
-              assetDTO,
-              'text',
-            );
-
-          expect(result).toBeDefined();
-          expect(result).toEqual(expectedResult);
-        });
-
-        it('should console log with error', async () => {
-          const assetDTO = new AssetDTO();
-          assetDTO.UserID = 1;
-          assetDTO.AssetID = '1';
-
-          mockTextractClient
-            .on(StartDocumentTextDetectionCommand)
-            .rejects(
-              new Error('Force Test Error'),
-            );
-          const mockConsole = jest.spyOn(
-            global.console,
-            'log',
-          );
-
-          await textractService.extractDocument(
-            'async',
-            assetDTO,
-            'text',
-          );
-
-          expect(mockConsole).toBeCalled();
-          mockConsole.mockRestore();
-        });
-      });
-
-      describe('Table Extraction', () => {
-        it('should return an extraction object', async () => {
-          const assetDTO = new AssetDTO();
-          assetDTO.UserID = 1;
-          assetDTO.AssetID = '1';
-
-          const expectedResult = {
-            Name: '1',
-            ExtractType: 'table',
-            Extracted: [{ Text: 'Inside Block' }],
-          };
-
-          const result =
-            await textractService.extractDocument(
-              'async',
-              assetDTO,
-              'table',
-            );
-
-          expect(result).toBeDefined();
-          expect(result).toEqual(expectedResult);
-        });
-
-        it('should console log with error', async () => {
-          const assetDTO = new AssetDTO();
-          assetDTO.UserID = 1;
-          assetDTO.AssetID = '1';
-
-          mockTextractClient
-            .on(StartDocumentAnalysisCommand)
-            .rejects(
-              new Error('Force Test Error'),
-            );
-          const mockConsole = jest.spyOn(
-            global.console,
-            'log',
-          );
-
-          await textractService.extractDocument(
-            'async',
-            assetDTO,
-            'table',
-          );
-
-          expect(mockConsole).toBeCalled();
-          mockConsole.mockRestore();
-        });
-      });
+      expect(result).toBeDefined();
     });
   });
 });
