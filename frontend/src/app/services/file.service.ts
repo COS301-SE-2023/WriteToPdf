2e27b72e3c132b2e1bd88ac489d4e9ec851be613
JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>
2023-08-15 14:41:29 +0200

    Temporarily removed document encryption

--- frontend/src/app/services/file.service.ts
+++ frontend/src/app/services/file.service.ts
@@ -479,6 +479,8 @@ export class FileService {
   }
 
   encryptDocument(content: string | undefined): string {
+    if(content)
+      return content;
     const key = this.userService.getEncryptionKey();
     if (key && content) {
       const encryptedMessage = CryptoJS.AES.encrypt(content, key).toString();
@@ -488,6 +490,8 @@ export class FileService {
     }
   }
   decryptDocument(content: string | undefined): string {
+    if (content)
+      return content;
     const key = this.userService.getEncryptionKey();
     if (key && content) {
       const decryptedMessage = CryptoJS.AES.decrypt(content, key)
