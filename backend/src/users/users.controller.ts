041df0ca939726990eb18a041265db0884118ccd
Janco <janco.spies@gmail.com>
2023-08-16 09:47:01 +0200

    Added basic google login endpoint

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -11,11 +11,13 @@ import { UsersService } from './users.service';
 import { UserDTO } from './dto/user.dto';
 import { Public } from '../auth/auth.controller';
 import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('users')
 export class UsersController {
   constructor(
     private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
   ) {}
 
   @Public()
@@ -98,5 +100,40 @@ export class UsersController {
     return this.usersService.signup(
       createUserDTO,
     );
+  }
+
+  @Public()
+  @HttpCode(HttpStatus.OK)
+  @Post('google_signin')
+  googleSignIn(
+    @Body() body: any,
+    @Req() request: Request,
+  ) {
+    const idToken = body.credential; // Assuming 'credential' field contains the ID token
+
+    // Decode the ID token using JwtService
+    const decodedToken = this.jwtService.decode(
+      idToken,
+    ) as {
+      email: string;
+      given_name: string;
+      family_name: string;
+    };
+
+    if (decodedToken) {
+      const { email, given_name, family_name } =
+        decodedToken;
+
+      console.log('Email:', email);
+      console.log('First Name:', given_name);
+      console.log('Last Name:', family_name);
+
+      return true; // Return any response you want
+    } else {
+      console.error(
+        'Invalid token or payload missing',
+      );
+      return false; // Return any response you want for error case
+    }
   }
 }
