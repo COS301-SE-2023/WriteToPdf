6d9dc6b43bb5b5ad8455c29b8fa854618d48f3ee
Janco <janco.spies@gmail.com>
2023-08-25 08:31:15 +0200

    Added missing repository import

--- backend/src/reset_password/reset_password.service.spec.ts
+++ backend/src/reset_password/reset_password.service.spec.ts
@@ -1,15 +1,32 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import {
+  Test,
+  TestingModule,
+} from '@nestjs/testing';
 import { ResetPasswordService } from './reset_password.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ResetPasswordRequest } from './entities/reset_password_request.entity';
 
 describe('ResetPasswordService', () => {
   let service: ResetPasswordService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ResetPasswordService],
-    }).compile();
+    const module: TestingModule =
+      await Test.createTestingModule({
+        providers: [
+          ResetPasswordService,
+          {
+            provide: getRepositoryToken(
+              ResetPasswordRequest,
+            ),
+            useClass: Repository,
+          },
+        ],
+      }).compile();
 
-    service = module.get<ResetPasswordService>(ResetPasswordService);
+    service = module.get<ResetPasswordService>(
+      ResetPasswordService,
+    );
   });
 
   it('should be defined', () => {
