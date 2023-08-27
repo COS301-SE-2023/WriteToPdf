0ae347df47390da34b0e2c1baec9226cc83e0674
Janco <janco.spies@gmail.com>
2023-08-27 16:48:37 +0200

    Added environment example file

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  frontendURL: 'http://localhost:4200/',
+  clientId: 'secret',
+  apiURL: 'http://localhost:3000/',
+  DEV_USER_EMAIL: 'secret',
+  DEV_USER_PASSWORD: 'secret'
+};
