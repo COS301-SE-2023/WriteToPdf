924451a0b15ac31d621f45c60e2c431350fcf016
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-09-15 16:55:33 +0200

    Added conceptual skeletion of what controller will offer

---
+++ backend/src/version_control/version_control.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common';
+import { VersionControlService } from './version_control.service';
+import { DiffDTO } from './dto/diff.dto';
+
+@Controller('version_control')
+export class VersionControlController {
+  constructor(
+    private readonly versionControlService: VersionControlService,
+  ) {}
+
+  @Post('save_diff')
+  saveDiff(@Body() diffDTO: DiffDTO) {
+    this.versionControlService.saveDiff(diffDTO);
+  }
+
+  @Post('get_diff')
+  getDiff(@Body() diffDTO: DiffDTO) {
+    return this.versionControlService.getDiff(
+      diffDTO,
+    );
+  }
+
+  @Post('get_all_diffs')
+  getAllDiffs() {
+    return this.versionControlService.getAllDiffs();
+  }
+
+  @Post('save_snapshot')
+  saveSnapshot() {
+    this.versionControlService.saveSnapshot();
+  }
+}
