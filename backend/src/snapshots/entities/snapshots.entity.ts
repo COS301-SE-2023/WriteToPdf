bc672bd2acb09c1bc8c4eefd6fc517920b780eeb
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-09-15 17:22:30 +0200

    Created SNAPSHOTS resource

---
+++ backend/src/snapshots/entities/snapshots.entity.ts
@@ -0,0 +1,20 @@
+// import {
+//   Entity,
+//   Column,
+//   PrimaryColumn,
+// } from 'typeorm';
+
+// @Entity('SNAPSHOTS')
+// export class SNAPSHOT {
+//   @PrimaryColumn()
+//   SnapshotID: string;
+
+//   @Column()
+//   MarkdownID: string;
+
+//   @Column()
+//   S3ArrayID: number;
+
+//   @Column()
+//   Timestamp: Date;
+// }
