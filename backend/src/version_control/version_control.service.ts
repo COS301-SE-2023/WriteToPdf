3a4e2b31f17337b216c0cd9b079b73581fe23ef2
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-09-15 16:21:17 +0200

    Added functions for finding logical order of circular array

--- backend/src/version_control/version_control.service.ts
+++ backend/src/version_control/version_control.service.ts
@@ -1,20 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { DiffDTO } from './dto/diff.dto';
 
 @Injectable()
 export class VersionControlService {
-  saveDiff() {}
+  saveDiff(diffDTO: DiffDTO) {}
 
-  getDiff() {}
+  getDiff(diffDTO: DiffDTO) {}
 
   getAllDiffs() {}
 
   //==-----------------------------------------------------
-
-  createSnapshot() {}
 
   saveSnapshot() {}
 
   getSnapshot() {}
 
   getAllSnapshots() {}
+
+  ///===----------------------------------------------------
+  // Helpers
+
+  getLogicalIndex(
+    idx: number,
+    head: number,
+    arr_len: number,
+  ): number {
+    return (idx - head + arr_len) % arr_len;
+  }
+
+  reorderArray(
+    arr: number[],
+    head: number,
+  ): number[] {
+    const logicalOrder: number[] = new Array(
+      arr.length,
+    ).fill(0);
+    for (let idx = 0; idx < arr.length; idx++) {
+      const logicalIndex = this.getLogicalIndex(
+        idx,
+        head,
+        arr.length,
+      );
+      logicalOrder[logicalIndex] = arr[idx];
+    }
+    return logicalOrder;
+  }
 }
