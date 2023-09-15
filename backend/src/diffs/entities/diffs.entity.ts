5d75fb6ce2c9ebf94e4df5444f827f05b62499e7
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-09-15 17:22:17 +0200

    Create DIFF resource

---
+++ backend/src/diffs/entities/diffs.entity.ts
@@ -0,0 +1,20 @@
+// import {
+//   Entity,
+//   Column,
+//   PrimaryColumn,
+// } from 'typeorm';
+
+// @Entity('DIFFS')
+// export class Diff {
+//   @PrimaryColumn()
+//   DiffID: string;
+
+//   @Column()
+//   MarkdownID: string;
+
+//   @Column()
+//   S3ArrayID: number;
+
+//   @Column()
+//   Timestamp: Date;
+// }
