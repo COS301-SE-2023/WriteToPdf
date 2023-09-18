516d8c8a2cf314e927c355aa3b94ed09d257d806
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-09-18 18:24:21 +0200

    Implemented load_history endpoint

--- backend/src/version_control/version_control.controller.ts
+++ backend/src/version_control/version_control.controller.ts
@@ -1,16 +1,25 @@
 import {
   Body,
   Controller,
+  HttpCode,
+  HttpStatus,
   Post,
 } from '@nestjs/common';
 import { VersionControlService } from './version_control.service';
 import { DiffDTO } from '../diffs/dto/diffs.dto';
 import { SnapshotDTO } from '../snapshots/dto/snapshot.dto';
+import { MarkdownFileDTO } from 'src/markdown_files/dto/markdown_file.dto';
+import { VersionHistoryDTO } from './dto/version_history.dto';
+import { version } from 'os';
+import { SnapshotService } from '../snapshots/snapshots.service';
+import { DiffsService } from '../diffs/diffs.service';
 
 @Controller('version_control')
 export class VersionControlController {
   constructor(
     private readonly versionControlService: VersionControlService,
+    private readonly snapshotService: SnapshotService,
+    private readonly diffService: DiffsService,
   ) {}
 
   ///===----------------------------------------------------
@@ -45,5 +54,54 @@ export class VersionControlController {
     this.versionControlService.saveSnapshot(
       snapshotDTO,
     );
+  }
+
+  ///===----------------------------------------------------
+
+  @Post('get_all_snapshots')
+  @HttpCode(HttpStatus.OK)
+  getAllSnapshots(
+    @Body() snapshotDTO: SnapshotDTO,
+  ) {
+    return this.versionControlService.getAllSnapshots(
+      snapshotDTO,
+    );
+  }
+
+  ///===----------------------------------------------------
+
+  @Post('load_history')
+  @HttpCode(HttpStatus.OK)
+  async loadHistory(
+    @Body() markdownFileDTO: MarkdownFileDTO,
+  ) {
+    const versionHistoryDTO =
+      new VersionHistoryDTO();
+
+    const snapshots =
+      await this.snapshotService.getAllSnapshots(
+        markdownFileDTO,
+      );
+
+    const snapshotDTOs =
+      await this.versionControlService.convertSnapshotsToSnapshotDTOs(
+        snapshots,
+      );
+
+    versionHistoryDTO.SnapshotHistory =
+      snapshotDTOs;
+
+    const diffs =
+      await this.diffService.getAllDiffs(
+        markdownFileDTO,
+      );
+
+    const diffDTOs =
+      await this.versionControlService.convertDiffsToDiffDTOs(
+        diffs,
+      );
+
+    versionHistoryDTO.DiffHistory = diffDTOs;
+    return versionHistoryDTO;
   }
 }
