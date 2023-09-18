0902137324465090e6195907c3b650b88c37ec8f
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-09-18 14:14:52 +0200

    Added function: createDiffs in database

--- backend/src/diffs/diffs.service.ts
+++ backend/src/diffs/diffs.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { Diff } from "./entities/diffs.entity";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Diff } from './entities/diffs.entity';
+import 'dotenv/config';
+import * as CryptoJS from 'crypto-js';
+import { MarkdownFileDTO } from '../markdown_files/dto/markdown_file.dto';
 
 @Injectable()
 export class DiffsService {
@@ -11,11 +14,43 @@ export class DiffsService {
   ) {}
 
   async updateDiff(nextDiffID: number) {
-    const diff = await this.diffRepository.findOne({
-      where: { S3DiffID: nextDiffID },
-    });
+    const diff =
+      await this.diffRepository.findOne({
+        where: { S3DiffID: nextDiffID },
+      });
 
     diff.LastModified = new Date();
     await this.diffRepository.save(diff);
+  }
+
+  async createDiffs(
+    markdownFileDTO: MarkdownFileDTO,
+  ) {
+    const diffRecords = [];
+    for (
+      let i = 0;
+      i < parseInt(process.env.MAX_DIFFS);
+      i++
+    ) {
+      const diffID = CryptoJS.SHA256(
+        markdownFileDTO.UserID.toString() +
+          new Date().getTime().toString() +
+          i.toString(),
+      ).toString();
+
+      diffRecords.push({
+        DiffID: diffID,
+        MarkdownID: markdownFileDTO.MarkdownID,
+        UserID: markdownFileDTO.UserID,
+        S3DiffID: i,
+      });
+    }
+    await this.diffRepository.insert(diffRecords);
+  }
+
+  async deleteDiffs(markdownFileDTO: MarkdownFileDTO) {
+    await this.diffRepository.delete({
+      MarkdownID: markdownFileDTO.MarkdownID,
+    });
   }
 }
