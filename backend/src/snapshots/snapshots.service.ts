a4a64081ad275677e2ed6c262c4f4f30e1af5f63
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-09-18 14:16:57 +0200

    Added function createSnapshots to create refs in db

--- backend/src/snapshots/snapshots.service.ts
+++ backend/src/snapshots/snapshots.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { Snapshot } from "./entities/snapshots.entity";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Snapshot } from './entities/snapshots.entity';
+import { MarkdownFileDTO } from '../markdown_files/dto/markdown_file.dto';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable()
 export class SnapshotService {
@@ -9,4 +11,39 @@ export class SnapshotService {
     @InjectRepository(Snapshot)
     private snapshotRepository: Repository<Snapshot>,
   ) {}
+
+  async createSnapshots(
+    markdownFileDTO: MarkdownFileDTO,
+  ) {
+    const snapshotRecords = [];
+    for (
+      let i = 0;
+      i < parseInt(process.env.MAX_SNAPSHOTS);
+      i++
+    ) {
+      const snapshotID = CryptoJS.SHA256(
+        markdownFileDTO.UserID.toString() +
+          new Date().getTime().toString() +
+          i.toString(),
+      ).toString();
+
+      snapshotRecords.push({
+        SnapshotID: snapshotID,
+        MarkdownID: markdownFileDTO.MarkdownID,
+        UserID: markdownFileDTO.UserID,
+        S3SnapshotID: i,
+      });
+    }
+    await this.snapshotRepository.insert(
+      snapshotRecords,
+    );
+  }
+
+  async deleteSnapshots(
+    markdownFileDTO: MarkdownFileDTO,
+  ) {
+    await this.snapshotRepository.delete({
+      MarkdownID: markdownFileDTO.MarkdownID,
+    });
+  }
 }
