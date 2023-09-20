7144608a7ce1e7af19c450c895705930e67e0db2
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-09-20 17:52:24 +0200

    Added various versioning functions

--- backend/src/version_control/version_control.controller.ts
+++ backend/src/version_control/version_control.controller.ts
@@ -13,6 +13,8 @@ import { VersionHistoryDTO } from './dto/version_history.dto';
 import { version } from 'os';
 import { SnapshotService } from '../snapshots/snapshots.service';
 import { DiffsService } from '../diffs/diffs.service';
+import { MarkdownFilesService } from '../markdown_files/markdown_files.service';
+import { VersionSetDTO } from './dto/version_set.dto';
 
 @Controller('version_control')
 export class VersionControlController {
@@ -20,6 +22,7 @@ export class VersionControlController {
     private readonly versionControlService: VersionControlService,
     private readonly snapshotService: SnapshotService,
     private readonly diffService: DiffsService,
+    private readonly markdownFileService: MarkdownFilesService,
   ) {}
 
   ///===----------------------------------------------------
@@ -28,42 +31,6 @@ export class VersionControlController {
   saveDiff(@Body() diffDTO: DiffDTO) {
     this.versionControlService.saveDiff(diffDTO);
   }
-
-  ///===----------------------------------------------------
-
-  // @Post('get_diff')
-  // getDiff(@Body() diffDTO: DiffDTO) {
-  //   return this.versionControlService.getDiff(
-  //     diffDTO,
-  //   );
-  // }
-
-  // ///===----------------------------------------------------
-
-  // @Post('get_all_diffs')
-  // getAllDiffs(@Body() snapshotDTO: SnapshotDTO) {
-  //   return this.versionControlService.getAllDiffsForSnapshot(
-  //     snapshotDTO,
-  //   );
-  // }
-
-  ///===----------------------------------------------------
-
-  // @Post('get_diff_set')
-  // getDiffSet(@Body() snapshotDTO: SnapshotDTO) {
-  //   return this.versionControlService.getDiffSetForSnapshot(
-  //     snapshotDTO,
-  //   );
-  // }
-
-  ///===----------------------------------------------------
-
-  // @Post('save_snapshot')
-  // saveSnapshot(@Body() snapshotDTO: SnapshotDTO) {
-  //   this.versionControlService.saveSnapshot(
-  //     snapshotDTO,
-  //   );
-  // }
 
   ///===----------------------------------------------------
 
@@ -79,6 +46,18 @@ export class VersionControlController {
 
   ///===----------------------------------------------------
 
+  @Post('get_history_set')
+  @HttpCode(HttpStatus.OK)
+  getHistorySet(
+    @Body() versionSetDTO: VersionSetDTO,
+  ) {
+    return this.versionControlService.getHistorySet(
+      versionSetDTO,
+    );
+  }
+
+  ///===----------------------------------------------------
+
   @Post('load_history')
   @HttpCode(HttpStatus.OK)
   async loadHistory(
@@ -87,30 +66,86 @@ export class VersionControlController {
     const versionHistoryDTO =
       new VersionHistoryDTO();
 
+    // Populate snapshot history
+
+    const nextSnapshotID =
+      await this.markdownFileService.getNextSnapshotID(
+        markdownFileDTO.MarkdownID,
+      );
+
     const snapshots =
       await this.snapshotService.getAllSnapshots(
         markdownFileDTO,
       );
 
-    const snapshotDTOs =
+    let snapshotDTOs =
       await this.versionControlService.convertSnapshotsToSnapshotDTOs(
         snapshots,
       );
 
+    if (
+      snapshotDTOs.length <
+      parseInt(process.env.MAX_SNAPSHOTS)
+    ) {
+      const baseSnapshotDTO = 
+        new SnapshotDTO();
+      baseSnapshotDTO.MarkdownID =
+        markdownFileDTO.MarkdownID;
+      baseSnapshotDTO.OldestSnapshot = true;
+      snapshotDTOs.unshift(baseSnapshotDTO);
+    }
+
+    if (
+      snapshotDTOs.length ===
+      parseInt(process.env.MAX_SNAPSHOTS)
+    ) {
+      snapshotDTOs =
+        await this.snapshotService.getLogicalSnapshotOrder(
+          snapshotDTOs,
+          nextSnapshotID,
+        );
+    }
+
     versionHistoryDTO.SnapshotHistory =
       snapshotDTOs;
 
-    const diffs =
-      await this.diffService.getAllDiffs(
-        markdownFileDTO,
+    // Populate diff history
+
+    const nextDiffID =
+      await this.markdownFileService.getNextDiffID(
+        markdownFileDTO.MarkdownID,
       );
 
-    const diffDTOs =
+    const diffs =
+      await this.diffService.getAllDiffs(
+        markdownFileDTO.MarkdownID,
+      );
+
+    let diffDTOs =
       await this.versionControlService.convertDiffsToDiffDTOs(
         diffs,
       );
 
+    if (
+      diffDTOs.length ===
+      parseInt(process.env.MAX_DIFFS)
+    ) {
+      diffDTOs =
+        await this.diffService.getLogicalDiffOrder(
+          diffDTOs,
+          nextDiffID,
+        );
+    }
+
     versionHistoryDTO.DiffHistory = diffDTOs;
+    //==-------------------------------
+    console.log(
+      'nextSnapshotID: ',
+      nextSnapshotID,
+    );
+    console.log('nextDiffID: ', nextDiffID);
+    console.log(versionHistoryDTO);
+    //==-------------------------------
     return versionHistoryDTO;
   }
 }
