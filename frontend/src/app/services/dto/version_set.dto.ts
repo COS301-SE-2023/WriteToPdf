c7b7f9512c3f55e73d76bc94397d906e88bb9b07
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-09-20 13:39:57 +0200

    Created backend version_set dto

--- frontend/src/app/services/dto/version_set.dto.ts
+++ frontend/src/app/services/dto/version_set.dto.ts
@@ -1,13 +1,13 @@
-export class VersionSetDTO  {
-    UserID: number;
-    MarkdownID: string;
-    DiffHistory: string[];
-    SnapshotID: string;
+export class VersionSetDTO {
+  UserID: number;
+  MarkdownID: string;
+  DiffHistory: string[];
+  SnapshotID: string;
 
-    constructor() {
-        this.UserID = 0;
-        this.MarkdownID = '';
-        this.DiffHistory = [];
-        this.SnapshotID = '';
-    }
+  constructor() {
+    this.UserID = 0;
+    this.MarkdownID = '';
+    this.DiffHistory = [];
+    this.SnapshotID = '';
+  }
 }
