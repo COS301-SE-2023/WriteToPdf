c047efaa166447161929b6704e0b89d06f38a41a
Janco <janco.spies@gmail.com>
2023-09-23 11:04:49 +0200

    Added diff tests

--- backend/src/diffs/diffs.service.spec.ts
+++ backend/src/diffs/diffs.service.spec.ts
@@ -9,9 +9,23 @@ import { MarkdownFileDTO } from '../markdown_files/dto/markdown_file.dto';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import {
-  isAfter,
+  isWithinInterval,
   subMilliseconds,
-} from 'date-fns'; // or any date manipulation library you prefer
+  addMilliseconds,
+} from 'date-fns';
+import * as CryptoJS from 'crypto-js';
+
+jest.mock('crypto-js', () => {
+  const mockedHash = jest.fn(
+    () => 'hashed string',
+  );
+
+  return {
+    SHA256: jest.fn().mockReturnValue({
+      toString: mockedHash,
+    }),
+  };
+});
 
 describe('DiffService', () => {
   let service: DiffsService;
@@ -76,7 +90,7 @@ describe('DiffService', () => {
       const updatedDiff = new Diff();
       updatedDiff.DiffID = '0';
       updatedDiff.HasBeenUsed = true;
-      updatedDiff.LastModified = new Date();
+      updatedDiff.LastModified = expect.any(Date);
 
       jest
         .spyOn(service, 'getDiff')
@@ -84,18 +98,7 @@ describe('DiffService', () => {
 
       jest
         .spyOn(Repository.prototype, 'save')
-        .mockImplementationOnce((diff) => {
-          const threshold = 1000;
-          const isDateCloseEnough = isAfter(
-            diff.LastModified,
-            subMilliseconds(
-              new Date(),
-              threshold,
-            ),
-          );
-          expect(isDateCloseEnough).toBe(true);
-          return Promise.resolve(diff);
-        });
+        .mockResolvedValue(updatedDiff);
 
       await service.updateDiff(
         diffDTO,
@@ -105,6 +108,39 @@ describe('DiffService', () => {
       expect(
         Repository.prototype.save,
       ).toBeCalledWith(updatedDiff);
+    });
+  });
+
+  describe('createDiff', () => {
+    it('should create a diff', async () => {
+      const markdownFileDTO =
+        new MarkdownFileDTO();
+      markdownFileDTO.UserID = 0;
+      markdownFileDTO.MarkdownID = '0';
+      markdownFileDTO.NextDiffIndex = 0;
+
+      const snapshotID = '0';
+
+      jest
+        .spyOn(Repository.prototype, 'insert')
+        .mockResolvedValueOnce(undefined);
+
+      await service.createDiff(
+        markdownFileDTO,
+        snapshotID,
+      );
+
+      expect(
+        Repository.prototype.insert,
+      ).toBeCalledWith({
+        DiffID: 'hashed string',
+        MarkdownID: markdownFileDTO.MarkdownID,
+        UserID: markdownFileDTO.UserID,
+        S3DiffIndex:
+          markdownFileDTO.NextDiffIndex,
+        HasBeenUsed: false,
+        SnapshotID: snapshotID,
+      });
     });
   });
 
@@ -124,6 +160,74 @@ describe('DiffService', () => {
       ).toBeCalledWith({
         MarkdownID: markdownDTO.MarkdownID,
       });
+    });
+  });
+
+  describe('resetDiffs', () => {
+    it('should update diffs', async () => {
+      const markdownID = '0';
+      const snapshotID = '0';
+
+      jest
+        .spyOn(Repository.prototype, 'update')
+        .mockResolvedValueOnce(undefined);
+
+      await service.resetDiffs(
+        markdownID,
+        snapshotID,
+      );
+
+      expect(
+        Repository.prototype.update,
+      ).toBeCalledWith(
+        {
+          MarkdownID: markdownID,
+          SnapshotID: snapshotID,
+        },
+        {
+          HasBeenUsed: false,
+        },
+      );
+    });
+  });
+
+  describe('getAllDiffs', () => {
+    it('should find all diffs', async () => {
+      const markdownID = '0';
+
+      const returnedDiffs = [
+        new Diff(),
+        new Diff(),
+      ];
+
+      jest
+        .spyOn(Repository.prototype, 'find')
+        .mockResolvedValueOnce(returnedDiffs);
+
+      const result = await service.getAllDiffs(
+        markdownID,
+      );
+
+      expect(result).toEqual(returnedDiffs);
+    });
+  });
+
+  describe('getLogicalIndex', () => {
+    it('should return a logical index', () => {
+      const s3Index = 0;
+      const nextDiffID = 0;
+      const arr_len = 0;
+
+      const result = service.getLogicalIndex(
+        s3Index,
+        nextDiffID,
+        arr_len,
+      );
+
+      expect(result).toBe(
+        (s3Index - nextDiffID + arr_len) %
+          arr_len,
+      );
     });
   });
 });
