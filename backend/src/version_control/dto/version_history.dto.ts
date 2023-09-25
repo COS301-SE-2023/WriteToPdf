0cb32e79ad5d1b119cd2c88174d2e6167a6a44ca
jakeweatherhead <jakeweatherhead1@gmail.com>
2023-09-25 20:02:09 +0200

    Added fields for restoration feature

--- backend/src/version_control/dto/version_history.dto.ts
+++ backend/src/version_control/dto/version_history.dto.ts
@@ -4,9 +4,15 @@ import { SnapshotDTO } from '../../snapshots/dto/snapshot.dto';
 export class VersionHistoryDTO {
   DiffHistory: DiffDTO[];
   SnapshotHistory: SnapshotDTO[];
+  RestorationDiffIndex: number;
+  RestorationContent: string;
 
   constructor() {
     this.DiffHistory = undefined;
     this.SnapshotHistory = undefined;
+    this.RestorationDiffIndex = undefined;
+    this.RestorationContent = undefined;
   }
 }
+
+// For simplicity sake, we can treat restoring to a snapshot the same as restoring to the last diff in that set.
