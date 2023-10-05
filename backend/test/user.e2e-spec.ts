a4dbc143ecbad60003bde1b359e5761e27a991f1
Janco <janco.spies@gmail.com>
2023-10-05 13:01:48 +0200

    Updated test error message

--- backend/test/user.e2e-spec.ts
+++ backend/test/user.e2e-spec.ts
@@ -80,7 +80,7 @@ describe('UserController (integration)', () => {
         .post('/users/login/')
         .send(requestUser);
       expect(response.body.error).toBe(
-        'User not found',
+        'Invalid credentials',
       );
       expect(response.status).toBe(
         HttpStatus.NOT_FOUND,
